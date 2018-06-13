fb2a8cb4e4d6a6976767db8844e3656cf84a2f05
Nahuel SG <nahuelsg_64@hotmail.com>
2018-06-13 10:53:49 -0300

    Contact form update

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
     	$("#loader").fadeOut("slow", function(){
 
         // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+        $("#preloader").delay(100).fadeOut("slow");
 
       });       
 
@@ -217,52 +217,52 @@
 	------------------------------------------------------ */
 
 	/* local validation */
-	$('#contactForm').validate({
-
-		/* submit via ajax */
-		submitHandler: function(form) {
-
-			var sLoader = $('#submit-loader');
-
-			$.ajax({      	
-
-		      type: "POST",
-		      url: "https://mailthis.to/NahuelGoldy",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
-  		}
-
-	});
+	// $('#contactForm').validate({
+    //
+	// 	/* submit via ajax */
+	// 	submitHandler: function(form) {
+    //
+	// 		var sLoader = $('#submit-loader');
+    //
+	// 		$.ajax({
+    //
+	// 	      type: "POST",
+	// 	      url: "https://mailthis.to/NahuelGoldy",
+	// 	      data: $(form).serialize(),
+	// 	      beforeSend: function() {
+    //
+	// 	      	sLoader.fadeIn();
+    //
+	// 	      },
+	// 	      success: function(msg) {
+    //
+	//             // Message was sent
+	//             if (msg == 'OK') {
+	//             	sLoader.fadeOut();
+	//                $('#message-warning').hide();
+	//                $('#contactForm').fadeOut();
+	//                $('#message-success').fadeIn();
+	//             }
+	//             // There was an error
+	//             else {
+	//             	sLoader.fadeOut();
+	//                $('#message-warning').html(msg);
+	// 	            $('#message-warning').fadeIn();
+	//             }
+    //
+	// 	      },
+	// 	      error: function() {
+    //
+	// 	      	sLoader.fadeOut();
+	// 	      	$('#message-warning').html("Something went wrong. Please try again.");
+	// 	         $('#message-warning').fadeIn();
+    //
+	// 	      }
+    //
+	//       });
+  	// 	}
+    //
+	// });
 
 
  	/*----------------------------------------------------- */
